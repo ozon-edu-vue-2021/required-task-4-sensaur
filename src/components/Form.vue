3678b539172e459156299f40de55f78b32b3bfa4
Ilya Zhigarev <zhigarev@bk.ru>
2021-11-10 17:36:51 +0300

    верстка

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,178 +1,274 @@
 <template>
-  <div class="form">
-    <div class="row">
-      {{ formData }}
-    </div>
-    <form autocomplete="off" class="form" @submit.prevent="formSubmit">
-      <h2>Личные данные</h2>
-
-      <div class="row">
-        <label for="surname"> Фамилия </label>
-        <input v-model="formData.surname" id="surname" />
-        <label for="name"> Имя </label>
-        <input v-model="formData.name" id="name" />
-        <label for="secondName"> Отчество </label>
-        <input v-model="formData.secondName" id="secondName" />
-      </div>
-
-      <div class="row">
-        <label for="birthDate"> Дата рождения </label>
-        <input
-          v-model="formData.birthDate"
-          id="birthDate"
-          placeholder="ДД.ММ.ГГГГ"
-        />
-      </div>
-
-      <div class="row">
-        <label for="email"> E-mail </label>
-        <input
-          v-model="formData.email"
-          id="email"
-          placeholder="test-email@mail.com"
-        />
-      </div>
-
-      <h2>Пол</h2>
-      <div class="row radio">
-        <input
-          v-model="formData.gender"
-          id="male"
-          type="radio"
-          name="gender"
-          value="Мужской"
-          checked
-        />
-        <label for="male"> Мужской </label>
-        <input
-          v-model="formData.gender"
-          id="female"
-          type="radio"
-          name="gender"
-          value="Женский"
-        />
-        <label for="female"> Женский </label>
-      </div>
-
-      <h2>Паспортные данные</h2>
-      <div class="row">
-        <div class="citizenship-selector" v-click-outside="hideDropdown">
-          <label for="citizenship"> Гражданство </label>
-          <input
-            id="citizenship"
-            @focus="isDropdownOpen = true"
-            v-model="formData.citizenship"
-          />
-          <div v-if="isDropdownOpen" class="citizenship-selector___dropdown">
-            <ul v-if="allCitizenship.length">
-              <li
-                v-for="citizenship in allCitizenship"
-                :key="citizenship._id"
-                @click="onCitizenshipClicked(citizenship)"
-              >
-                {{ citizenship.nationality }}
-              </li>
-            </ul>
-            <div v-else class="empty">Ничего на найдено</div>
+  <div class="container">
+    <div class="form">
+      <form autocomplete="off" class="form" @submit.prevent="formSubmit">
+        <h2>Личные данные</h2>
+        <div class="row">
+          <div class="col">
+            <label for="surname" class="form-label"> Фамилия </label>
+            <input
+              v-model="formData.surname"
+              id="surname"
+              class="form-control"
+            />
+          </div>
+          <div class="col">
+            <label for="name" class="form-label"> Имя </label>
+            <input v-model="formData.name" id="name" class="form-control" />
+          </div>
+          <div class="col">
+            <label for="secondName" class="form-label"> Отчество </label>
+            <input
+              v-model="formData.secondName"
+              id="secondName"
+              class="form-control"
+            />
           </div>
         </div>
-
-        <div>
-          <div v-if="isPassportRussian">
-            <label for="passportRusSerial"> Серия паспорта </label>
+        <div class="row">
+          <div class="col-sm-3">
+            <label for="birthDate" class="form-label"> Дата рождения </label>
             <input
-              id="passportRusSerial"
-              v-model="formData.passportRusSerial"
-            />
-            <label for="passportRusNumber"> Номер паспорта </label>
-            <input
-              id="passportRusNumber"
-              v-model="formData.passportRusNumber"
-            />
-            <label for="passportRusIssueDate"> Дата выдачи</label>
-            <input
-              id="passportRusIssueDate"
+              v-model="formData.birthDate"
+              id="birthDate"
               placeholder="ДД.ММ.ГГГГ"
-              v-model="formData.passportRusIssueDate"
+              class="form-control"
             />
-          </div>
-          <div v-else>
-            <div class="row">
-              <label for="latinSurname"> Фамилия на латинице</label>
-              <input v-model="formData.latinSurname" id="latinSurname" />
-              <label for="latinName"> Имя на латинице </label>
-              <input v-model="formData.latinName" id="latinName" />
-            </div>
-            <div class="row">
-              Иностранцы заполняют латинскими буквами. Например Ivanov Ivan
-            </div>
-            <label for="passportForeignNumber"> Номер паспорта </label>
-            <input
-              id="passportForeignNumber"
-              v-model="formData.passportForeignNumber"
-            />
-            <label for="passportForeignIssueCountry"> Страна выдачи </label>
-            <input
-              id="passportForeignIssueCountry"
-              v-model="formData.passportForeignIssueCountry"
-            />
-
-            <label for="passportForeignType"> Тип паспорта </label>
-            <input
-              id="passportForeignType"
-              list="passportForeignTypeList"
-              name="passportForeignType"
-              v-model="formData.passportForeignType"
-            />
-            <datalist id="passportForeignTypeList">
-              <option
-                v-for="passportTypes in allPassportTypes"
-                :key="passportTypes.id"
-              >
-                {{ passportTypes.type }}
-              </option>
-            </datalist>
-          </div>
-          <div class="row radio" @change="onNameChangedClicked()">
-            <div>Меняли ли фамилию или имя</div>
-            <input
-              v-model="formData.nameChanged"
-              id="no"
-              type="radio"
-              name="nameChanged"
-              value="Нет"
-            />
-            <label for="no"> Нет </label>
-            <input
-              v-model="formData.nameChanged"
-              id="yes"
-              type="radio"
-              name="nameChanged"
-              value="Да"
-              checked
-            />
-            <label for="yes"> Да </label>
-          </div>
-          <div v-if="isNameChanged">
-            <label for="previousSurname"> Предыдущая фамилия </label>
-            <input v-model="formData.previousSurname" id="previousSurname" />
-            <label for="previousName"> Предыдущее имя </label>
-            <input v-model="formData.previousName" id="previousName" />
           </div>
         </div>
-        <button>Отправить</button>
-      </div>
-    </form>
+        <div class="row">
+          <div class="col-sm-12">
+            <label for="email" class="form-label"> E-mail </label>
+            <input
+              v-model="formData.email"
+              id="email"
+              placeholder="test-email@mail.com"
+              class="form-control"
+            />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12">
+            Пол
+            <div class="form-check">
+              <input
+                v-model="formData.gender"
+                id="male"
+                type="radio"
+                name="gender"
+                value="Мужской"
+                class="form-check-input"
+                checked
+              />
+              <label for="male" class="form-check-label"> Мужской </label>
+              <div>
+                <input
+                  v-model="formData.gender"
+                  id="female"
+                  type="radio"
+                  name="gender"
+                  value="Женский"
+                  class="form-check-input"
+                />
+                <label for="female" class="form-check-label"> Женский </label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <h2>Паспортные данные</h2>
+        <div class="row">
+          <div class="col-sm-12">
+            <!--            <select class="form-select" aria-label="Default select example">-->
+            <!--              <option selected>Гражданство тест</option>-->
+            <!--              <option value="1">One</option>-->
+            <!--              <option value="2">Two</option>-->
+            <!--              <option value="3">Three</option>-->
+            <!--            </select>-->
+
+            <div class="form-select" v-click-outside="hideDropdown">
+              <label for="citizenship" class="form-label"> Гражданство </label>
+              <input
+                id="citizenship"
+                @focus="isDropdownOpen = true"
+                v-model="formData.citizenship"
+                class="form-control"
+              />
+              <div v-if="isDropdownOpen">
+                <ul v-if="allCitizenship.length">
+                  <li
+                    v-for="citizenship in allCitizenship"
+                    :key="citizenship._id"
+                    @click="onCitizenshipClicked(citizenship)"
+                  >
+                    {{ citizenship.nationality }}
+                  </li>
+                </ul>
+                <div v-else class="empty">Ничего на найдено</div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div v-if="isPassportRussian">
+              <div class="row">
+                <div class="col-sm-3">
+                  <label for="passportRusSerial"> Серия паспорта </label>
+                  <input
+                    id="passportRusSerial"
+                    v-model="formData.passportRusSerial"
+                    class="form-control"
+                  />
+                </div>
+                <div class="col-sm-3">
+                  <label for="passportRusNumber"> Номер паспорта </label>
+                  <input
+                    id="passportRusNumber"
+                    v-model="formData.passportRusNumber"
+                    class="form-control"
+                  />
+                </div>
+                <div class="col-sm-6">
+                  <label for="passportRusIssueDate"> Дата выдачи</label>
+                  <input
+                    id="passportRusIssueDate"
+                    placeholder="ДД.ММ.ГГГГ"
+                    v-model="formData.passportRusIssueDate"
+                    class="form-control"
+                  />
+                </div>
+              </div>
+            </div>
+            <div v-else>
+              <div class="row">
+                <div class="col-sm-6">
+                  <label for="latinSurname"> Фамилия на латинице</label>
+                  <input
+                    v-model="formData.latinSurname"
+                    id="latinSurname"
+                    class="form-control"
+                  />
+                </div>
+                <div class="col-sm-6">
+                  <label for="latinName"> Имя на латинице </label>
+                  <input
+                    v-model="formData.latinName"
+                    id="latinName"
+                    class="form-control"
+                  />
+                </div>
+              </div>
+              <div class="form-text">
+                Иностранцы заполняют латинскими буквами. Например Ivanov Ivan
+              </div>
+              <div class="row">
+                <div class="col-sm-4">
+                  <label for="passportForeignNumber"> Номер паспорта </label>
+                  <input
+                    id="passportForeignNumber"
+                    v-model="formData.passportForeignNumber"
+                    class="form-control"
+                  />
+                </div>
+                <div class="col-sm-4">
+                  <label for="passportForeignIssueCountry">
+                    Страна выдачи
+                  </label>
+                  <input
+                    id="passportForeignIssueCountry"
+                    v-model="formData.passportForeignIssueCountry"
+                    class="form-control"
+                  />
+                </div>
+                <div class="col-sm-4">
+                  <label for="passportForeignType"> Тип паспорта </label>
+                  <input
+                    id="passportForeignType"
+                    list="passportForeignTypeList"
+                    name="passportForeignType"
+                    v-model="formData.passportForeignType"
+                    class="form-select"
+                  />
+                  <datalist id="passportForeignTypeList">
+                    <option
+                      v-for="passportTypes in allPassportTypes"
+                      :key="passportTypes.id"
+                    >
+                      {{ passportTypes.type }}
+                    </option>
+                  </datalist>
+                </div>
+              </div>
+            </div>
+
+            <div class="row radio" @change="onNameChangedClicked()">
+              <div class="row">
+                <div class="col-sm-12">
+                  Меняли ли фамилию или имя
+                  <div class="form-check">
+                    <input
+                      v-model="formData.nameChanged"
+                      id="no"
+                      type="radio"
+                      name="nameChanged"
+                      value="Нет"
+                      class="form-check-input"
+                      checked
+                    />
+                    <label for="no"> Нет </label>
+                    <div>
+                      <input
+                        v-model="formData.nameChanged"
+                        id="yes"
+                        type="radio"
+                        name="nameChanged"
+                        value="Да"
+                        class="form-check-input"
+                      />
+                      <label for="yes"> Да </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div v-if="isNameChanged" class="row">
+              <div class="col-sm-6">
+                <label for="previousSurname"> Предыдущая фамилия </label>
+                <input
+                  v-model="formData.previousSurname"
+                  id="previousSurname"
+                  class="form-control"
+                />
+              </div>
+              <div class="col-sm-6">
+                <label for="previousName"> Предыдущее имя </label>
+                <input
+                  v-model="formData.previousName"
+                  id="previousName"
+                  class="form-control"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="row-cols-sm-5">
+            <button class="btn btn-success">Отправить</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+// const bootstrap = require('bootstrap')
 import ClickOutside from "vue-click-outside";
 import citizenship from "../../src/assets/data/citizenships.json";
 import passportTypes from "../../src/assets/data/passport-types.json";
 
 // console.log(citizenship);
 // console.log(passportTypes);
+// console.log(bootstrap);
 
 export default {
   data() {
@@ -187,7 +283,6 @@ export default {
         email: "",
         gender: "",
         citizenship: "",
-        // passportTypes: "",
         passportRusSerial: "",
         passportRusNumber: "",
         passportForeignNumber: "",
@@ -230,17 +325,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.form {
-  border-radius: 6px;
-  border: 1px solid #ccd8e4;
-  background: white;
-  max-width: 1000px;
-  margin: 0 auto;
-  padding: 40px;
-  overflow: auto;
-}
-
-.citizenship-selector___dropdown {
-}
-</style>
+<style scoped></style>
