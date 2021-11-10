51099c7e7f0f137c9eb2a1fafff2a86d815b5e9f
Ilya Zhigarev <zhigarev@bk.ru>
2021-11-10 10:22:24 +0300

    start

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,31 +1,24 @@
 <template>
   <div class="form">
-    <form
-      autocomplete="off"
-      class="form"
-    >
-      <h2> Личные данные </h2>
-      <div class="row">
-        {{ formData }}
-      </div>
+    <div class="row">
+      {{ formData }}
+    </div>
+    <form autocomplete="off" class="form">
+      <h2>Личные данные</h2>
+
       <div class="row">
         <label for="surname"> Фамилия </label>
-        <input
-          id="surname"
-        />
+        <input v-model="formData.surname" id="surname" />
         <label for="name"> Имя </label>
-        <input
-          id="name"
-        />
+        <input v-model="formData.name" id="name" />
         <label for="secondName"> Отчество </label>
-        <input
-          id="secondName"
-        />
+        <input v-model="formData.secondName" id="secondName" />
       </div>
 
       <div class="row">
         <label for="birthDate"> Дата рождения </label>
         <input
+          v-model="formData.birthDate"
           id="birthDate"
           placeholder="ДД.ММ.ГГГГ"
         />
@@ -40,50 +33,49 @@
         />
       </div>
 
-      <h2> Пол </h2>
-
+      <h2>Пол</h2>
       <div class="row radio">
         <input
+          v-model="formData.gender"
+          id="male"
           type="radio"
+          name="gender"
+          value="Мужской"
+          checked
         />
-        <span for="gender"> Мужской </span>
+        <label for="male"> Мужской </label>
         <input
+          v-model="formData.gender"
+          id="female"
           type="radio"
+          name="gender"
+          value="Женский"
         />
-        <span for="gender"> Женский </span>
+        <label for="female"> Женский </label>
       </div>
 
-
-      <h2> Паспортные данные </h2>
+      <h2>Паспортные данные</h2>
       <div class="row">
-        <div class="skill-selector"
-             v-click-outside="hideDropdown">
-          <label for="skills"> Гражданство </label>
+        <div class="citizenship-selector" v-click-outside="hideDropdown">
+          <label for="citizenship"> Гражданство </label>
           <input
-            id="skills"
+            id="citizenship"
             @focus="isDropdownOpen = true"
+            v-model="formData.citizenship"
           />
 
-          <div
-            v-if="isDropdownOpen"
-            class="skill-selector___dropdown"
-          >
-            <ul v-if="allSkills.length">
+          <div v-if="isDropdownOpen" class="citizenship-selector___dropdown">
+            <ul v-if="allCitizenship.length">
               <li
-                v-for="skill in allSkills"
-                :key="skill._id"
-                @click="onSkillClicked(skill)"
+                v-for="citizenship in allCitizenship"
+                :key="citizenship._id"
+                @click="onCitizenshipClicked(citizenship)"
               >
-                {{ skill.nationality }}
+                {{ citizenship.nationality }}
               </li>
             </ul>
-            <div
-              v-else
-              class="empty">
-              Ничего на найдено
-            </div>
+            <div v-else class="empty">Ничего на найдено</div>
           </div>
-
         </div>
       </div>
     </form>
@@ -92,34 +84,38 @@
 
 <script>
 import ClickOutside from "vue-click-outside";
-import skills from "../../src/assets/data/citizenships.json";
+import citizenship from "../../src/assets/data/citizenships.json";
 
-console.log(skills);
+console.log(citizenship);
 
 export default {
   data() {
     return {
       formData: {
+        surname: "",
+        name: "",
+        secondName: "",
+        birthDate: "",
         email: "",
+        gender: "",
         citizenship: "",
-        skills: []
       },
       isDropdownOpen: false,
-      allSkills: skills
+      allCitizenship: citizenship,
     };
   },
   methods: {
     hideDropdown() {
       this.isDropdownOpen = false;
     },
-    onSkillClicked(selectedSkill) {
-      this.formData.skills.push(selectedSkill);
-      this.hideDropdown()
-    }
+    onCitizenshipClicked(selectedCitizenship) {
+      this.formData.citizenship = selectedCitizenship.nationality;
+      this.hideDropdown();
+    },
   },
   directives: {
-    ClickOutside
-  }
+    ClickOutside,
+  },
 };
 </script>
 
@@ -132,5 +128,7 @@ export default {
   margin: 0 auto;
   padding: 40px;
   overflow: auto;
+}
+.citizenship-selector___dropdown {
 }
 </style>
